7b6f752722692271117e2580e0f790544e6b09c7
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-13 23:46:33 +0530

    Added Signup and Storing user in firebase

---
+++ javascripts/auth.js
@@ -0,0 +1,15 @@
+// Sign Up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    }) //asynchronous task
+})
