826ceff944a6eaa7d41a5194a6201dc0c5767c1f
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-13 23:59:10 +0530

    Added Login and LogOut Features

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -13,3 +13,27 @@ signupForm.addEventListener('submit', (e) => {
         signupForm.reset();
     }) //asynchronous task
 })
+
+//logout method
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('User signed out');
+    });
+});
+
+//login method
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    
+    auth.signInWithEmailAndPassword(email, password).then( cred => {
+        //close the modal
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+})
