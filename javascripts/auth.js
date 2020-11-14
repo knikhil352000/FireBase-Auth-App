0b8e549ff42ff245ffcf87f91cb5925fc44a1d53
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-14 21:44:33 +0530

    Securing the cloud functions and handled login errors

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -69,6 +69,9 @@ signupForm.addEventListener('submit', (e) => {
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
+        signupForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        signupForm.querySelector('.error').innerHTML = err.message;
     }) //asynchronous task
 })
 
@@ -95,6 +98,9 @@ loginForm.addEventListener('click', (e) => {
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         loginForm.reset();
+        loginForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        loginForm.querySelector('.error').innerHTML = err.message;
     });
 });
 
