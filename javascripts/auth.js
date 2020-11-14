da36177132cb78d660ff58759fad32dbf480bd6b
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-14 11:40:53 +0530

    Adding Data to firestore by user

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -13,7 +13,25 @@ auth.onAuthStateChanged(user => {
         setupUI();
         setupGuides([]);
     }
-})
+});
+
+//Create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        //close the modal
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log('You are not authenticated');
+    });
+});
 
 
 // Sign Up
